a9d57e724a9006c1dae72e5f22ff34d9a353f102
Yoonjong Lee <l2yoonjong@gmail.com>
2025-06-29 14:12:48 +0900

    last commmit

--- blog/src/App.js
+++ blog/src/App.js
@@ -8,18 +8,27 @@ import userEvent from '@testing-library/user-event';
 function App() {
 
   let post = '강남 우동 맛집';
+  let [가나다, 가나다순정렬] = useState(list);
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학'])
   let [따봉, 따봉변경] = useState(0);
-  
+
+  function orderTitle() {
+    let newOrder = [...글제목].sort();
+    setTitle(newOrder);
+  }
+
   return (
     <div className="App">
       <div class="black-nav">
         <h4>블로그임</h4>
       </div>
 
-      <button onClick={() => {
-        글제목변경(['여자 코트 추천', '강남 우동 맛집', '파이썬독학'])
+      <button onClick={ orderTitle }>가나다순정렬</button>
 
+      <button onClick={() => {
+				let copy = [...글제목];
+				copy[0] = '여자 코트 추천';
+				글제목변경(copy);
       }}>글수정</button>
       <div className="list">
         <h4>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1) } }>👍</span> { 따봉 } </h4> 
