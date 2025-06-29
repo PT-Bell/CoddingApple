bae18d015c71f8753cf64fc28da1a443a700cb4b
Yoonjong Lee <l2yoonjong@gmail.com>
2025-06-29 15:10:01 +0900

    UPDATE app

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,33 +3,32 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-import userEvent from '@testing-library/user-event';
 
 function App() {
 
   let post = '강남 우동 맛집';
-  let [가나다, 가나다순정렬] = useState(list);
-  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학'])
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학']);
   let [따봉, 따봉변경] = useState(0);
 
   function orderTitle() {
     let newOrder = [...글제목].sort();
-    setTitle(newOrder);
+    글제목변경(newOrder);
   }
 
   return (
     <div className="App">
-      <div class="black-nav">
+      <div className="black-nav">
         <h4>블로그임</h4>
       </div>
 
       <button onClick={ orderTitle }>가나다순정렬</button>
 
       <button onClick={() => {
-				let copy = [...글제목];
-				copy[0] = '여자 코트 추천';
-				글제목변경(copy);
+        let copy = [...글제목];
+        copy[0] = '여자 코트 추천';
+        글제목변경(copy);
       }}>글수정</button>
+
       <div className="list">
         <h4>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1) } }>👍</span> { 따봉 } </h4> 
         <p>2월 17일 발행</p>
